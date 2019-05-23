bac3691b7b4017c1dd40739874f7bb9c28aa9cfc
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-05-23 15:12:12 -0500

    Proyecto Finalizado v1.0

---
+++ precache-manifest.330448aad0447fe41f7d6de8b02d6d28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ac0e648beaee6dfd463cb0294600de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f5e750fb206eecbef1e",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2f31b4ccf299a63fefc5",
+    "url": "/static/js/2.32cc53d5.chunk.js"
+  },
+  {
+    "revision": "3f5e750fb206eecbef1e",
+    "url": "/static/js/main.fe975593.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
